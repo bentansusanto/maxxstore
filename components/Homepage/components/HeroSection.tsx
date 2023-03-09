7a568409621f81b2d23855c6f053580c6481f0a8
bentansusanto <tansusanto194@gmail.com>
2023-03-09 21:33:37 +0700

    update hero section in homepage, and responsive

--- components/Homepage/components/HeroSection.tsx
+++ components/Homepage/components/HeroSection.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../../../styles/Home.module.css'
-import MediaQuery from '../../mediaQuery/MediaQuery'
 
-const HeroSection = () => {
-    const isMobile = MediaQuery("(max-width: 600px)")
+
+const HeroSection = ({isMobile} : {isMobile:boolean}) => {
+    
   return (
     <div>
         <div className={styles.hero}>
-            <div>
-                
+            <div className={` ${isMobile ? "pt-32 p-6" : "pt-52 pl-10"} space-y-5`}>
+                <h1 className={`${isMobile ? "text-4xl w-[95%]" : "text-4xl w-[35%]"} capitalize `}>Picked Every Item With Care, <span className='font-bold'>You must try.</span></h1>
+                <button className='bg-black py-3 rounded-md px-6 text-white'>
+                  <Link href={'/'}>View Collection</Link>
+                </button>
             </div>
         </div>
     </div>
--- components/Homepage/index.tsx
+++ components/Homepage/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from './components/HeroSection'
+import MediaQuery from '../mediaQuery/MediaQuery'
 
 const Home = () => {
+  const isMobile = MediaQuery("(max-width: 600px)")
   return (
     <div>
-        <HeroSection/>
+        <HeroSection isMobile={isMobile}/>
     </div>
   )
 }
